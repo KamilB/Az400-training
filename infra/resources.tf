950c0877b1ae0f35beb72f03a359adc41ebab950
KamilB <kamilbedkowski85@gmail.com>
2021-04-29 22:22:51 +0200

    syntax

--- infra/resources.tf
+++ infra/resources.tf
@@ -8,6 +8,7 @@ resource "azurerm_app_service_plan" "az400serviceplan" {
   location            = azurerm_resource_group.az400terraform.location
   resource_group_name = azurerm_resource_group.az400terraform.name
   kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Standard"
